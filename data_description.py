a2f89bf0d5c54f942f7a5a31a7f4a1959acd5dce
Daniel-Prus <dlpruszynski@o2.pl>
2022-02-21 14:54:07 +0100

    update

--- data_description.py
+++ data_description.py
@@ -15,5 +15,20 @@ class MRRowsNumber(MRJob):
         yield 'rows number', sum(values)
 
 
+# get unique airlines codes
+class MRAirlines(MRJob):
+    def mapper(self, _, line):
+        (fl_date, op_carrier, op_carrier_fl_num, origin, dest, crs_dep_time, dep_time, dep_delay, taxi_out, wheels_off,
+         wheels_on, taxi_in, crs_arr_time, arr_time, arr_delay, cancelled, cancellation_code, diverted,
+         crs_elapsed_time, actual_elapsed_time, air_time, distance, carrier_delay, weather_delay, nas_delay,
+         security_delay, late_aircraft_delay, unnamed) = line.split(",")
+
+        yield op_carrier, None
+
+    def reducer(self, airlines, values):
+        yield airlines, None
+
+
 if __name__ == "__main__":
-    MRRowsNumber.run()
+    MRAirlines.run()
+    # MRRowsNumber.run()
